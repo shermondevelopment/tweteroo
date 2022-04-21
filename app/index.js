aaa28c5ee1c158055c6409db2655980b6662bcce
shermondevelopment <victorshermon@yahoo.com>
2022-04-21 08:58:52 -0300

    including setting of cors in app

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
-import express from 'express'
+import express, { json } from 'express'
+
+/* cors */
+import cors from './middlewares/cors'
 
 const app = express()
 
+app.use(json())
+app.use(cors())
 
 app.listen(5000, () => console.log('app running in port 5000 🚀🚀🚀🚀'))
