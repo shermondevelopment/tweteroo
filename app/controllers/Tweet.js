329c9bb492fd5b8412734b06b6a19ba66afdc10f
shermondevelopment <victorshermon@yahoo.com>
2022-04-21 10:23:20 -0300

    including feature of list last ten tweets

--- app/controllers/Tweet.js
+++ app/controllers/Tweet.js
@@ -1,9 +1,12 @@
 const tweets = []
 
-const TweetsController = (req, res) => {
+export const TweetsController = (req, res) => {
   const { username, tweet } = req.body
   tweets.push({ username, tweet })
   res.status(201).send('OK')
 }
 
-export default TweetsController
+export const TweetsReadController = (req, res) => {
+  const lastTenTweets = tweets.slice(-10)
+  res.status(200).json(lastTenTweets)
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 
 import SignUpController from '../controllers/SignUp.js'
-import TweetsController from '../controllers/Tweet.js'
+import {TweetsController, TweetsReadController} from '../controllers/Tweet.js'
 
 const router = Router()
 
 router.post('/sign-up', SignUpController);
 router.post('/tweets', TweetsController)
+router.get('/tweets', TweetsReadController)
 
 
 export default router
