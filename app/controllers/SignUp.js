2b9fdc0fca583c2cbf415cfacc7aec3a9edb43d6
shermondevelopment <victorshermon@yahoo.com>
2022-04-21 10:55:41 -0300

    validate input empty or undefined in signup

--- app/controllers/SignUp.js
+++ app/controllers/SignUp.js
@@ -4,7 +4,7 @@ export let profile
 
 export const SignUpController = (req, res) => {
   const { username, avatar } = req.body
-  if(username.length <= 0 || avatar.length <= 0) {
+  if(!username || !avatar) {
     return res.status(400).send('Todos os campos são obrigatórios!')
   }
   profile = avatar;
