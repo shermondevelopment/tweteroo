54292e3c76b3ed52828e361b76eac69791672cb1
shermondevelopment <victorshermon@yahoo.com>
2022-04-21 10:54:41 -0300

    validate input empty or undefined in tweets

--- app/controllers/SignUp.js
+++ app/controllers/SignUp.js
@@ -4,6 +4,9 @@ export let profile
 
 export const SignUpController = (req, res) => {
   const { username, avatar } = req.body
+  if(username.length <= 0 || avatar.length <= 0) {
+    return res.status(400).send('Todos os campos são obrigatórios!')
+  }
   profile = avatar;
   myUsers.push({ username, avatar })
   res.status(201).send('OK')
--- app/controllers/Tweet.js
+++ app/controllers/Tweet.js
@@ -4,6 +4,9 @@ const tweets = []
 
 export const TweetsController = (req, res) => {
   const { username, tweet } = req.body
+  if(!username || !tweet) {
+    return res.status(400).send('Todos os campos são obrigatórios!')
+  }
   tweets.push({ username, avatar: profile, tweet })
   res.status(201).send('OK')
 }
