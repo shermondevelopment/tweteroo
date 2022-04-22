90ef73973804f1d3b6f7f0597752fc7142145e57
shermondevelopment <victorshermon@yahoo.com>
2022-04-21 21:20:42 -0300

    receiving user for header of request

--- app/controllers/Tweet.js
+++ app/controllers/Tweet.js
@@ -3,11 +3,12 @@ import { profile } from "./SignUp.js"
 const tweets = []
 
 export const TweetsController = (req, res) => {
-  const { username, tweet } = req.body
-  if(!username || !tweet) {
+  const { tweet } = req.body
+  const { user } = req.headers
+  if(!user || !tweet) {
     return res.status(400).send('Todos os campos são obrigatórios!')
   }
-  tweets.push({ username, avatar: profile, tweet })
+  tweets.push({ username: user, avatar: profile, tweet })
   res.status(201).send('OK')
 }
 
